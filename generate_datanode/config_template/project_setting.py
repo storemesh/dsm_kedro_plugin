4e06cf05478977562efa4f0391b0d7f2af57987c
patcharapon1995 <patcharapon1995@gmail.com>
2022-12-26 17:14:27 +0700

    [update] config template

--- generate_datanode/config_template/project_setting.py
+++ generate_datanode/config_template/project_setting.py
@@ -1,8 +1,8 @@
-PROJECT_NAME = "tc-06" 
-PROJECT_FOLDER_ID = 10
+PROJECT_NAME = "<YOUR_PROJECT_NAME>" # (It must be exactly the same value in Project Table of Data Discovery, use it for generate logs)  
+PROJECT_FOLDER_ID: <YOUR_PROJECT_FOLDER_ID>
+DATAPLATFORM_API_URI = "<YOUR_DATAPLATFORM_API_URI>" # example: "https://api.discovery.dev.data.storemesh.com"
+OBJECT_STORAGE_URI = "<YOUR_OBJECT_STORAGE_URI>" # example: "dataframe.objectstorage.dev.data.storemesh.com"
 
-DATAPLATFORM_API_URI = "https://api.discovery.dev.data.storemesh.com"
-OBJECT_STORAGE_URI = "dataframe.objectstorage.dev.data.storemesh.com"
+OBJECT_STORAGE_SECUE = <True or False> # use True if Object Storage connect with https
 
 REQUIRED_FOLDER_LIST = ["SQLDataNode", "Landing", "Staging", "Integration", "Logs"]
-
