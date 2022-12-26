0c30402c4470d3860913440576c8c7b39e1f325d
patcharapon1995 <patcharapon1995@gmail.com>
2022-12-26 17:17:26 +0700

    [update] project setting

--- generate_datanode/config_template/project_setting.py
+++ generate_datanode/config_template/project_setting.py
@@ -1,5 +1,5 @@
 PROJECT_NAME = "<YOUR_PROJECT_NAME>" # (It must be exactly the same value in Project Table of Data Discovery, use it for generate logs)  
-PROJECT_FOLDER_ID: <YOUR_PROJECT_FOLDER_ID>
+PROJECT_FOLDER_ID = <YOUR_PROJECT_FOLDER_ID>
 DATAPLATFORM_API_URI = "<YOUR_DATAPLATFORM_API_URI>" # example: "https://api.discovery.dev.data.storemesh.com"
 OBJECT_STORAGE_URI = "<YOUR_OBJECT_STORAGE_URI>" # example: "dataframe.objectstorage.dev.data.storemesh.com"
 
